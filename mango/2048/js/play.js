c6c878400d0a4b0f0974f40a062c55c23040b3bd
Nathaniel Schwartz <gnat79@gmail.com>
2018-07-20 11:17:28 -0400

    Update 2048 "app"

--- mango/2048/js/play.js
+++ mango/2048/js/play.js
@@ -1,27 +1,35 @@
+// New tiles are either 2 or 4, with this probability of being 2
+let probabilityOfRolling2 = .9;
+
 // The game board [row][col]
-let board = [];
+let board = [ ];
 
 let score = 0;
 
-// Background, foreground
-let textColor = "#E5E1D3";
+// Text colors
+let lightColor = "#E5E1D3";
+let darkColor = "#3D3C38";
+
+// Tile colors
 let colors = {
-    2: "#CAD2C5",
-    4: "#84A98C",
-    8: "#52796F",
-    16: "#354F52",
-    32: "#2F3E46",
-    64: "#5171A5",
-    128:"#36558F",
-    256: "#4C2C69",
-    512: "#42253B",
-    1024: "#832232",
-    2048: "#963D5A",
-    4096: "#C16E70",
-    8192: "#9E5E31",
-    16384: "#FF6F59",
+    2: {"bgColor": "#CAD2C5", "fgColor": darkColor},
+    4: {"bgColor": "#84A98C", "fgColor": darkColor},
+    8: {"bgColor": "#52796F", "fgColor": lightColor},
+    16: {"bgColor": "#354F52", "fgColor": lightColor},
+    32: {"bgColor": "#2F3E46", "fgColor": lightColor},
+    64: {"bgColor": "#5171A5", "fgColor": lightColor},
+    128: {"bgColor": "#36558F", "fgColor": lightColor},
+    256: {"bgColor": "#4C2C69", "fgColor": lightColor},
+    512: {"bgColor": "#42253B", "fgColor": lightColor},
+    1024: {"bgColor": "#832232", "fgColor": lightColor},
+    2048: {"bgColor": "#963D5A", "fgColor": lightColor},
+    4096: {"bgColor": "#C16E70", "fgColor": lightColor},
+    8192: {"bgColor": "#9E5E31", "fgColor": darkColor},
+    16384: {"bgColor": "#FF6F59", "fgColor": darkColor},
 };
 
+
+// This function handles key presses and mouse clicks.
 $().ready(function () {
     // Initialize the board to empty.
     for (let j = 0; j < 4; j++) {
@@ -32,50 +40,61 @@ $().ready(function () {
         board.push(row);
     }
 
+    // Start the game with two random tiles.
+    startGame();
+
     // Add a tile where clicked, or else double the value of the clicked tile.
     $("#game_board").on('mousedown', 'div.tile', function () {
-        if($(this).hasClass("active")) {
+        if ($(this).hasClass("active")) {
             let [row, col] = getPosition($(this));
             board[row][col] *= 2;
             updateTile($(this));
-
         } else {
             let [row, col] = getPosition($(this));
-            addTile(row, col);
+            addTile(row, col, Math.pow(2, getRandomInt(1, 4)));
         }
         return false;
     });
 
+    // Restart the game.
+    $("#restart").mousedown(function (e) {
+        e.preventDefault();
+        resetBoard();
+        score = 0;
+        updateScoreDisplay();
+        startGame();
+        return false;
+    });
+
     // Handle arrow keys
-    $(document).keydown(function(event) {
+    $(document).keydown(function (event) {
         let keyCode = event.which;
         switch (keyCode) {
             case 37: {
                 if (slideTiles("left")) {
-                    addRandomTiles();
-                }
+                    addRandomTile();
+                } else if (gameOver()) endGame();
                 event.preventDefault();
                 break;
             }
             case 38: {
                 if (slideTiles("up")) {
-                    addRandomTiles();
-                }
+                    addRandomTile();
+                } else if (gameOver()) endGame();
                 event.preventDefault();
                 break;
             }
             case 39: {
                 if (slideTiles("right")) {
-                    addRandomTiles();
-                }
+                    addRandomTile();
+                } else if (gameOver()) endGame();
                 event.preventDefault();
                 break;
             }
             case 40: {
                 if (slideTiles("down")) {
-                    addRandomTiles();
-                    alert("Moved DOWN");
-                }
+                    addRandomTile();
+                } else if (gameOver()) endGame();
                 event.preventDefault();
                 break;
             }
@@ -84,7 +103,6 @@ $().ready(function () {
         }
         return false;
     });
-
     return false;
 });
 
@@ -93,30 +111,27 @@ function updateTile($tile) {
     let value = board[row][col];
     if (value < 16385) {
         $tile.text(value);
-        $tile.css("background-color", colors[value]);
-        if (value !== 2) $tile.css("color", textColor);
+        $tile.css("background-color", colors[value]['bgColor']);
+        $tile.css("color", colors[value]['fgColor']);
     }
-    return false;
 }
 
-function addTile(row, col) {
+function addTile(row, col, value) {
     let $newTile = $('<div class="tile"></div>');
     $newTile.addClass("row" + row + " col" + col);
     $newTile.toggleClass("active holder");
-    let value = 2;
     board[row][col] = value;
     $newTile.text(board[row][col]);
-    $newTile.css("background-color", colors[value]);
-    $newTile.css("color", "#3D3C38");
+    $newTile.css("background-color", colors[value]['bgColor']);
+    $newTile.css("color", colors[value]['fgColor']);
     $("#game_board").append($newTile);
-    return false;
 }
 
 function slideTiles(direction) {
     let movedTiles = false;
     switch (direction) {
         case "down": {
-            for (let row = 1; row >= 0; row++) {
+            for (let row = 1; row < 4; row++) {
                 for (let col = 0; col < 4; col++) {
                     if (board[row][col] > 0) {
                         let $tile = getTileAtPosition(row, col);
@@ -124,7 +139,8 @@ function slideTiles(direction) {
                     }
                 }
             }
-        } break;
+        }
+            break;
         case "up": {
             for (let row = 2; row >= 0; row--) {
                 for (let col = 0; col < 4; col++) {
@@ -134,7 +150,8 @@ function slideTiles(direction) {
                     }
                 }
             }
-        } break;
+        }
+            break;
         case "left": {
             for (let col = 1; col < 4; col++) {
                 for (let row = 0; row < 4; row++) {
@@ -144,7 +161,8 @@ function slideTiles(direction) {
                     }
                 }
             }
-        } break;
+        }
+            break;
         case "right": {
             for (let col = 2; col >= 0; col--) {
                 for (let row = 0; row < 4; row++) {
@@ -179,6 +197,8 @@ function removeTileAtPosition(row, col) {
 function updateScoreDisplay() {
     let scoreDiv = $("#score")[0];
     scoreDiv.innerHTML = "Score: " + score;
+    setBestScore(score);
+    $("#best")[0].innerHTML = "Best: " + Cookies.get('highScore');
 }
 
 function slideTile($tile, direction) {
@@ -273,32 +293,98 @@ function slideTile($tile, direction) {
     }
 }
 
+// Move a tile to the specified cell.
 function moveTileToPosition($tile, currentRow, currentCol, newRow, newCol) {
+    let playerWon = false;
     if (currentCol !== newCol || currentRow !== newRow) {
         let currentVal = board[currentRow][currentCol];
         let newVal = board[newRow][newCol];
         if (currentVal === newVal) {
             let doubleVal = currentVal * 2;
+            if (doubleVal === 2048) playerWon = true;
             board[newRow][newCol] = doubleVal;
             score += doubleVal;
             removeTileAtPosition(newRow, newCol);
             updateScoreDisplay();
             updateTile($tile);
-        } else board[newRow][newCol] = currentVal;
+        } else {
+            board[newRow][newCol] = currentVal;
+        }
         board[currentRow][currentCol] = 0;
         $tile.removeClass("row" + currentRow + " col" + currentCol);
         $tile.addClass("row" + newRow + " col" + newCol);
         updateTile($tile);
     }
+    if (playerWon) alert("You won! Keep playing?");
 }
 
-function addRandomTiles() {
-    let numTilesToAdd = getRandomInt(1,3);
-    alert(numTilesToAdd);
+// Add a single tile (randomly) to one of the empty cells. Value of new tile is either 2 or 4 with probability of
+// being 2 set by the constant probabilityOfRolling2
+function addRandomTile() {
+    let row;
+    let col;
+    let emptyCell = false;
+    while (!emptyCell) {
+        row = getRandomInt(0, 4);
+        col = getRandomInt(0, 4);
+        emptyCell = (board[row][col] === 0);
+    }
+    let value = 2;
+    let prob = Math.random();
+    if (prob > probabilityOfRolling2) value = 4;
+    addTile(row, col, value);
 }
 
+// The maximum is exclusive and the minimum is inclusive. Stolen (and then edited) from somewhere on the internets.
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Play continues as long as there are no empty cells and no two adjacent cells are equal.
+function gameOver() {
+    // Check for equal tiles in any column or row
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] === 0
+                || board[j][i] === 0
+                || board[i][j] === board[i][j + 1]
+                || board[j][i] === board[j + 1][i]
+            ) return false;
+        }
+        if (board[i][3] === 0 || board[3][i] === 0) return false;
+    }
+    return true;
+}
+
+// Eventually: add an opaque cover to the board with a "Play Again?" button.
+function endGame() {
+    alert("Game is over");
+}
+
+// Adds random tiles to board.
+function startGame() {
+    updateScoreDisplay();
+    addRandomTile();
+    addRandomTile();
+}
+
+// Remove all tiles from the board.
+function resetBoard() {
+    for (let row = 0; row < 4; row++) {
+        for (let col = 0; col < 4; col++) {
+            removeTileAtPosition(row, col);
+            board[row][col] = 0;
+        }
+    }
+}
+
+// Set "best" score as a cookie
+function setBestScore(score) {
+    let currentBest = Cookies.get('highScore');
+    if (currentBest === undefined) {
+        Cookies.set('highScore', score);
+    }
+    else if (currentBest < score) {
+        Cookies.set('highScore', score);
+    }
 }
--- mango/mango/settings.py
+++ mango/mango/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/mango/static/'
 
 # Configure Django App for Heroku.
 import django_heroku
